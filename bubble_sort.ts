0e405d4027b6078ad708dbda4850d47764c5b786
CCnova <caiocnova1@gmail.com>
2023-08-04 13:38:20 -0300

    Add bubble sort

---
+++ bubble_sort.ts
@@ -0,0 +1,35 @@
+function swap(arr: any[], i: number, j: number) {
+  const temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+/**
+ * Bubble sort algorithm
+ * The main idea is to compare each element with the next one
+ * and swap them if the next one is smaller (out of order). We
+ * also keeps track if a swap was done in the current iteration
+ * this indicates that a new iteration is necessary. Each iteration
+ * moves the biggest element in this iteration to the right most
+ * position.
+ *
+ * @param arr Unsorted array to be sorted
+ */
+function bubbleSort(arr: number[]): number[] {
+  let lastElementPositioned = arr.length - 1;
+  let sorted = false;
+  while (!sorted) {
+    sorted = true;
+    for (let i = 0; i < lastElementPositioned; i++) {
+      if (arr[i] > arr[i + 1]) {
+        swap(arr, i, i + 1);
+        sorted = false;
+      }
+    }
+    lastElementPositioned--;
+  }
+
+  return arr;
+}
+
+console.log(bubbleSort([5, 4, 3, 2, 1]));
